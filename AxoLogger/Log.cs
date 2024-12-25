e8078d0ad6e5d5052eb92d7b5c738c41bae20ed9
AXOLOTsh <96595812+AXOLOTsh@users.noreply.github.com>
2024-12-26 02:09:58 +0300

    Add project files.

---
+++ AxoLogger/Log.cs
@@ -0,0 +1,48 @@
+﻿namespace AxoLogger {
+    public enum LogLevel { Info, Debug, Error, Warning }
+    public class Log {
+        public string Message { get; private set; }
+        public string Sender { get; private set; }
+        public LogLevel Level { get; private set; }
+        public DateTime CreationTime { get; private set; }
+
+        public Log() { }
+        public Log(string log, string sender = "", LogLevel level = LogLevel.Info) {
+            Message = log;
+            Sender = sender;
+            Level = level;
+
+            CreationTime = DateTime.Now;
+        }
+
+        public override string ToString() => GetFullLogMessage();
+
+        public string GetFullLogMessage() => $"[green]{CreationTime}[/] - {GetLogLevelString()} [gray]{GetSender()}:[/] {Message}";
+
+        private string GetSender() {
+            if (Sender != "")
+                return $"({Sender})";
+            return "";
+        }
+        private string GetLogLevelString() => GetLogLevelColorString(GetLogLevelName());
+        private string GetLogLevelColorString(string content) => $"[{GetLogLevelColor()}][[{content}]][/]";
+        private string GetLogLevelColor() {
+            switch (Level) {
+                case LogLevel.Info: return "blue";
+                case LogLevel.Debug: return "fuchsia";
+                case LogLevel.Error: return "red";
+                case LogLevel.Warning: return "yellow";
+            }
+            return "";
+        }
+        private string GetLogLevelName() {
+            switch (Level) {
+                case LogLevel.Info: return "Info";
+                case LogLevel.Debug: return "Debug";
+                case LogLevel.Error: return "Error";
+                case LogLevel.Warning: return "Warning";
+            }
+            return "Unknown";
+        }
+    }
+}
---
+++ AxoLogger/Logger.cs
@@ -0,0 +1,30 @@
+﻿using Spectre.Console;
+
+namespace AxoLogger {
+    public delegate void onLogDelegate(Log log);
+    public class Logger {
+        public event onLogDelegate onLog;
+        public string Name { get; private set; }
+        public IReadOnlyList<Log> Logs => _logs;
+        private List<Log> _logs = new List<Log>();
+
+        public Logger() { }
+        public Logger(string name, bool useDefaultLogger = true) {
+            Name = name;
+            if (useDefaultLogger) {
+                onLog += DefaultLogger;
+            }
+        }
+
+        public Log Log(string message, LogLevel level = LogLevel.Info) {
+            var log = new Log(message, Name, level);
+            Log(log);
+            return log;
+        }
+        public void Log(Log log) => onLog.Invoke(log);
+
+        public static void DefaultLogger(Log log) {
+            AnsiConsole.MarkupLine(log.GetFullLogMessage());
+        }
+    }
+}
